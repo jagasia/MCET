15957e15578ee4cc0e60be1439b8eb786636404b
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-11-02 14:32:48 +0530

    02-Nov 14:32

---
+++ spring/nanda/src/main/java/org/mcet/demo/NandaApplication.java
@@ -0,0 +1,14 @@
+package org.mcet.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NandaApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
