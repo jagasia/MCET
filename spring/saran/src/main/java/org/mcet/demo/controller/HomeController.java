b8b9cb075f0a32f6d9e8a5e0e623525048759c99
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2023-11-03 09:39:39 +0530

    03-Nov 09:39

--- spring/saran/src/main/java/org/mcet/demo/controller/HomeController.java
+++ spring/saran/src/main/java/org/mcet/demo/controller/HomeController.java
@@ -1,19 +1,27 @@
 package org.mcet.demo.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+
+import javax.servlet.annotation.MultipartConfig;
 
 import org.mcet.demo.entity.Product;
 import org.mcet.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+//@CrossOrigin({"*","http://localhost:4200"})
+@MultipartConfig
 public class HomeController {
 	@Autowired
 	private ProductRepository pr;
@@ -28,18 +36,28 @@ public class HomeController {
 		return mv;
 	}
 	
+//	@RequestMapping(value = "/btn", method = RequestMethod.POST, params = "add" )
+////	@ResponseBody
+//	public ModelAndView add(Product product)
+//	{
+//		pr.save(product);
+//		return home();
+//	}
+	
 	@RequestMapping(value = "/btn", method = RequestMethod.POST, params = "add" )
 //	@ResponseBody
-	public ModelAndView add(Product product)
+	public ModelAndView add(@RequestParam("name") String name,@RequestParam("price") Double price,@RequestParam("pic") MultipartFile file) throws IOException
 	{
+		Product product=new Product(null, name, price, file.getBytes());
 		pr.save(product);
 		return home();
 	}
 	
 	@RequestMapping(value = "/btn", method = RequestMethod.POST, params = "update" )
 //	@ResponseBody
-	public ModelAndView update(Product product)
+	public ModelAndView update(@RequestParam("id") Integer id,@RequestParam("name") String name,@RequestParam("price") Double price,@RequestParam("pic") MultipartFile file) throws IOException
 	{
+		Product product=new Product(id, name, price, file.getBytes());
 		Optional<Product> temp = pr.findById(product.getId());
 		Product p=null;
 		if(temp.isPresent())
@@ -52,13 +70,13 @@ public class HomeController {
 	
 	@RequestMapping(value = "/btn", method = RequestMethod.POST, params = "delete" )
 //	@ResponseBody
-	public ModelAndView delete(Product product)
+	public ModelAndView delete(@RequestParam("id") Integer id)
 	{
-		Optional<Product> temp = pr.findById(product.getId());
+		Optional<Product> temp = pr.findById(id);
 		Product p=null;
 		if(temp.isPresent())
 		{
-			p=product;
+			p=temp.get();
 			pr.delete(p);
 		}
 		return home();
--- spring/saran/src/main/webapp/WEB-INF/jsp/index.jsp
+++ spring/saran/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -24,10 +24,11 @@
     </div>
     <div class="col-sm-4">
       <h3>Product page</h3>
-     <form method="post" action="/btn">
+     <form method="post" action="/btn" enctype="multipart/form-data">
      	Id: <input type="number" name="id" class="form-control" />
      	Name: <input type="text" name="name" class="form-control" />
      	Price: <input type="number" name="price" class="form-control" />
+     	Pic: <input type="file" name="pic" class="form-control" />
      	<br/>
      	<input type="submit" name="add" value="Add" class="btn btn-success" />
      	<input type="submit" name="update" value="Update" class="btn btn-warning" />
@@ -47,7 +48,7 @@
 	<div class="col-sm-8">
 		<table class="table table-bordered table-hover table-striped">
 			<thead>
-				<th>Id</th><th>Name</th><th>Price</th><th></th>
+				<th>Id</th><th>Name</th><th>Price</th><th></th><th></th>
 			</thead>
 			<tbody>
 				<c:forEach var="p" items="${products }">
@@ -55,6 +56,9 @@
 						<td>${p.id }</td>
 						<td>${p.name }</td>
 						<td>${p.price }</td>
+						<td>
+							<img src="data:image/jpg;base64,${p.pic1 }" width="80" />
+						</td>
 						<td>Select</td>
 					</tr>
 				</c:forEach>
